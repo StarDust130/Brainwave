64d83d9e88c4726a81d9462e89020d3c0ef8d7c2
ChandraShekhar <csyadav0513@gmail.com>
2024-03-27 16:42:55 +0530

    Collaboration Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benifits from "./components/Benifits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benifits />
+        <Collaboration />
       </div>
 
       <ButtonGradient />
